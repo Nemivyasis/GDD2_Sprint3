52268fd5a2490c8497aed6db7184e1d9a7bb3c13
Vincent-DiCocco <vincent.dicocco@hotmail.com>
2018-11-12 20:10:11 -0500

    New Scenes and The door

---
+++ GDD2_Sprint3/Assets/Scripts/victoryCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class victoryCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collide)
+    {
+        if(collide.gameObject.tag == "Player")
+        {
+            if(SceneManager.GetActiveScene().buildIndex < SceneManager.GetAllScenes().Length)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
